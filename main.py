d0a27c7b813063cc0f5243d1ec93149da811692d
lusbyc <43541354+lusbyc@users.noreply.github.com>
2021-12-03 04:07:13 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+print()
+print("Uppercase Alphabet:")
+letter = 65
+while letter in range(65,91):
+    print(chr(letter),end=' ')
+    letter += 1
+
+print()
+print()
+
+print("Lowercase Alphabet:")
+letter = 97
+while letter in range(97,123):
+    print(chr(letter),end=' ')
+    letter += 1
+print()
+print()
